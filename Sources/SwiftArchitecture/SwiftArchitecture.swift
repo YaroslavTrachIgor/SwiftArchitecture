4b3c71ec14962d482a4af77a158cbe62ae96b3ba
Yaroslav Trach <zhbr282@gmail.com>
2024-12-28 01:56:56 -0500

    Add New BaseSwiftUIView protocol

--- Sources/SwiftArchitecture/SwiftArchitecture.swift
+++ Sources/SwiftArchitecture/SwiftArchitecture.swift
@@ -19,13 +19,6 @@ import SwiftUI
 //MARK: - ()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()-()
 ///****************************************************************************************************************************************************************************
 
-
-public protocol ViewProtocol {
-    associatedtype ViewModelType
-    var viewModel: ViewModelType { get set }
-    init(viewModel: ViewModelType)
-}
-
 public protocol ViewModelProtocol {
     associatedtype InteractorType
     associatedtype RouterType
@@ -73,17 +66,17 @@ struct Injected<T> {
 }
 
 public final class ModuleAssembler {
-    @MainActor public static func assemble<V: Sendable, P, I, R>(
+    @MainActor public static func assemble<V, P, I, R>(
         view: V.Type,
         viewModel: P.Type,
         interactor: I.Type,
         router: R.Type
-    ) -> P where
-    V: ViewProtocol,
+    ) -> V where
+    V: BaseSwiftUIViewProtocol,
     I: InteractorProtocol,
     P: ViewModelProtocol,
     R: RouterProtocol,
-    V.ViewModelType == P,
+    V.ViewModel == P,
     P.InteractorType == I,
     P.RouterType == R,
     I.ViewModelType == P,
@@ -100,7 +93,7 @@ public final class ModuleAssembler {
         interactor.viewModel = viewModel
         router.viewModel = viewModel
         
-        return viewModel
+        return view
     }
 }
 
@@ -117,18 +110,30 @@ public final class ModuleAssembler {
 
 
 
+public protocol BaseSwiftUIViewProtocol {
+    associatedtype ViewModel
+    init (viewModel: ViewModel)
+}
+
+public typealias BaseSwiftUIView = View & BaseSwiftUIViewProtocol
+
+
+
+
+
+
+
+
 
 @MainActor
-open class BaseViewController<VM>: @preconcurrency ViewProtocol {
+open class ViewModel<I, R>: @preconcurrency ViewModelProtocol {
     
-    public var viewModel: VM
+    public let interactor: I
+    public let router: R
     
-    required public init(viewModel: VM) {
-        self.viewModel = viewModel
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    required public init(interactor: I, router: R) {
+        self.interactor = interactor
+        self.router = router
     }
 }
 
@@ -139,149 +144,15 @@ open class BaseInteractor<VM: AnyObject>: @preconcurrency InteractorProtocol {
     required public init() {}
 }
 
-
-
-
-
-
-
-
-
-
-
-@MainActor
-public final class DependencyContainer {
-    private var dependencies: [String: Any] = [:]
-    public static let shared = DependencyContainer()
-    
-    public func register<T>(_ type: T.Type, instance: T) {
-        let key = String(describing: type)
-        dependencies[key] = instance
-    }
-    
-    public func resolve<T>() -> T {
-        let key = String(describing: T.self)
-        guard let instance = dependencies[key] as? T else {
-            fatalError("No dependency found for \(T.self)")
-        }
-        return instance
-    }
-}
-
-
-@propertyWrapper
-public struct Observable<Value> {
-    private var value: Value
-    private var observers = [(Value) -> Void]()
-    
-    public init(wrappedValue: Value) {
-        self.value = wrappedValue
-    }
-    
-    public var wrappedValue: Value {
-        get { value }
-        set {
-            value = newValue
-            notifyObservers()
-        }
-    }
-    
-    public var projectedValue: Observable<Value> {
-        get { self }
-        set { self = newValue }
-    }
-    
-    public mutating func bind(_ observer: @escaping (Value) -> Void) {
-        observers.append(observer)
-        observer(value)
-    }
-    
-    private func notifyObservers() {
-        observers.forEach { $0(value) }
-    }
-}
-
-@MainActor
-public protocol NavigationCoordinator: AnyObject {
-    func start()
-    func coordinate(to coordinator: NavigationCoordinator)
-    func finish()
-}
-
-@MainActor
-open class BaseCoordinator: NavigationCoordinator {
-    private var childCoordinators: [NavigationCoordinator] = []
-    
-    public func start() {}
-    
-    public func coordinate(to coordinator: NavigationCoordinator) {
-        childCoordinators.append(coordinator)
-        coordinator.start()
-    }
-    
-    public func finish() {
-        childCoordinators.removeAll()
-    }
-}
-
 @MainActor
 open class BaseRouter<VM: AnyObject>: @preconcurrency RouterProtocol {
     public weak var viewModel: VM?
-    public weak var navigationController: UINavigationController?
-    private var coordinator: BaseCoordinator?
     
     required public init() {}
-    
-    public func setNavigationController(_ nav: UINavigationController) {
-        self.navigationController = nav
-    }
-    
-    public func setCoordinator(_ coordinator: BaseCoordinator) {
-        self.coordinator = coordinator
-    }
-    
-    public func push<T: UIViewController>(_ viewController: T, animated: Bool = true) {
-        navigationController?.pushViewController(viewController, animated: animated)
-    }
-    
-    public func pop(animated: Bool = true) {
-        navigationController?.popViewController(animated: animated)
-    }
-    
-    public func present<T: UIViewController>(_ viewController: T, animated: Bool = true) {
-        navigationController?.present(viewController, animated: animated)
-    }
 }
 
 
-@MainActor
-open class ViewModel<I, R>: @preconcurrency ViewModelProtocol {
-    public let interactor: I
-    public let router: R
-    
-    private(set) var isLoading = false
-    private(set) var error: Error?
-    
-    required public init(interactor: I, router: R) {
-        self.interactor = interactor
-        self.router = router
-    }
-    
-    public func setLoading(_ loading: Bool) {
-        isLoading = loading
-    }
-    
-    public func setError(_ error: Error?) {
-        self.error = error
-    }
-}
 
-struct NavigationControllerRepresentable: UIViewControllerRepresentable {
-    let navigationController: UINavigationController
-    
-    func makeUIViewController(context: Context) -> UINavigationController {
-        navigationController
-    }
-    
-    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {}
-}
+
+
+
